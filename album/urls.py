8ae097440aed570710257707e30d930e75863332
jihyunniiii <jita1125@naver.com>
2023-07-05 22:16:31 +0900

    [chore] url 수정

--- album/urls.py
+++ album/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name="album"
 urlpatterns = [
     path('', views.album_read_create),
-    path('<int:album_id>', views.album_update_delete),
-    path('<int:album_id>/track', views.track_read_create),
-    path('track/<int:track_id>', views.track_update_delete),
+    path('albums/<int:album_id>', views.album_update_delete),
+    path('albums/<int:album_id>/tracks', views.track_read_create),
+    path('tracks/<int:track_id>', views.track_update_delete),
 ]
